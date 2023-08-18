aabedc0e6a1927a2cb08bf04066d2d834917fa8b
Gabriel Santos <82126990+Lucio072@users.noreply.github.com>
2023-08-18 04:06:51 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'administrativo/156-belem',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
